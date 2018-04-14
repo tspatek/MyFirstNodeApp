a1d09aef98a7c2bc2d7af5e6eb8ded0cbd20723c
Tiffany Patek <tiffanypatek@Tiffanys-MacBook-Pro.local>
2018-04-13 22:07:50 -0600

    add spotify function

--- liri.js
+++ liri.js
@@ -1,14 +1,14 @@
 require("dotenv").config();
 
 const Twitter = require("twitter");
-const Spotify = require('node-spotify-api');
+const Spotify = require("node-spotify-api");
 const keys = require("./keys.js");
 
 const client = new Twitter(keys.twitter);
 const spotify = new Spotify(keys.spotify);
 
-
 const command = process.argv[2];
+let name = process.argv[3];
 
 switch (command) {
     case "my-tweets":
@@ -28,13 +28,15 @@ switch (command) {
 function showMyTweets() {
 
     let params = {
-        screen_name: 'AhsokaT38777354',
-        count: "20" 
+        screen_name: "AhsokaT38777354",
+        count: "20"
     };
     client.get('statuses/user_timeline', params, function (error, tweets, response) {
         if (!error) {
-            tweets.forEach(function(item) {
-                console.log(`Created: ${item.created_at} Tweet: ${item.text}`);
+            tweets.forEach(function (item) {
+                console.log(`\nCreated: ${item.created_at}\n` +
+                            `Tweet: ${item.text}\n`
+                );
             })
         } else {
             console.log(error);
@@ -44,7 +46,26 @@ function showMyTweets() {
 }
 
 function spotifyThisSong() {
+    if (!name) {
+        name = "The Sign";
+    }
 
+    spotify.search({ type: 'track', query: name }, function (error, data) {
+        if (!error) {
+            let tracks = data.tracks.items;
+            tracks.forEach(function (song) {
+                if (name === song.name) {
+                    console.log(`\nArtist: ${song.artists[0].name}\n` +
+                                `Name: ${song.name}\n` +
+                                `Preview: ${song.preview_url}\n` +
+                                `Album: ${song.album.name}\n`
+                    );
+                }
+            })
+        } else {
+            console.log(error);
+        }
+    });
 }
 
 function movieThis() {
